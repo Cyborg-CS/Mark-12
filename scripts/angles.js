3aede58a738dfb0154d71732729cc1a4aaaa7df9
DevMD <81987904+Cyborg-CS@users.noreply.github.com>
2022-06-17 13:11:35 +0530

    added: isTriangle

---
+++ scripts/angles.js
@@ -0,0 +1,26 @@
+const userAngles = document.querySelectorAll(".input-angles");
+const checkBtn = document.querySelector("#check-btn");
+const resultBox = document.querySelector("#output-tag");
+
+
+const calculateAngles =(angleOne, angleTwo, angleThree)=>{
+  let sum = angleOne + angleTwo + angleThree;
+  return sum;
+}
+
+function checkTriangle(){
+   let sum = calculateAngles(
+    Number(userAngles[0].value),
+    Number(userAngles[1].value),
+    Number(userAngles[2].value)
+   ) 
+   if(sum === 180){
+    console.log("Way to go ! Your angles are forming a Triangle.")
+    resultBox.innerHTML ="Way to go ! Your angles are making a Triangle.";
+   }else{
+    console.log("Oops ! Looks like you can't form a Triangle with is angles.")
+    resultBox.innerHTML ="Oops ! Looks like you can't form a Triangle with is angles."
+   }
+}
+
+checkBtn.addEventListener('click', checkTriangle);
