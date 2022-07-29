f659f9b69405060b6cece337d2550c3a87b69a66
DevMD <81987904+Cyborg-CS@users.noreply.github.com>
2022-07-29 20:21:57 +0530

    quiz.js added

---
+++ scripts/quiz.js
@@ -0,0 +1,34 @@
+const questionsForm = document.querySelector(".quiz-section");
+const submitBtn = document.querySelector(".check-btn");
+const outputBox = document.querySelector("#output-tag");
+
+const answers = ["90°",
+"right angled",
+"one right angle",
+"12, 16, 20",
+"Equilateral triangle",
+"100°",
+"30°",
+"a + b + c",
+"no",
+"45°"];
+
+
+function calculateFinalScore(){
+    const formValues = new FormData(questionsForm);
+    let score = 0, index= 0;
+    for(let values of formValues.values()){
+        if(values === answers[index]){
+        score = score + 1;
+        }
+        index = index+ 1;
+    }
+
+   outputBox.innerText = "Your Final Score is "+score; 
+}
+
+
+
+
+
+submitBtn.addEventListener('click', calculateFinalScore);
