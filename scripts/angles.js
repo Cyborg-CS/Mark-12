9371b3dc55a7a9cdafad3b9387fd63a65faf43b3
DevMD <81987904+Cyborg-CS@users.noreply.github.com>
2022-09-07 16:14:28 +0530

    fix: negative and zeros

--- scripts/angles.js
+++ scripts/angles.js
@@ -4,8 +4,13 @@ const resultBox = document.querySelector("#output-tag");
 
 
 const calculateAngles =(angleOne, angleTwo, angleThree)=>{
+  
+  if(angleOne > 0 && angleTwo > 0 && angleThree > 0){
   let sum = angleOne + angleTwo + angleThree;
   return sum;
+}else{
+  resultBox.innerText = "Angle's has to be more than 0 degree."
+}
 }
 
 function checkTriangle(){
@@ -14,13 +19,15 @@ function checkTriangle(){
     Number(userAngles[1].value),
     Number(userAngles[2].value)
    ) 
+   if(sum){
    if(sum === 180){
     console.log("Way to go ! Your angles are forming a Triangle.")
     resultBox.innerHTML ="Way to go ! Your angles are making a Triangle.";
    }else{
     console.log("Oops ! Looks like you can't form a Triangle with is angles.")
     resultBox.innerHTML ="Oops ! Looks like you can't form a Triangle with this angles."
-   }
+   };
+  };
 }
 
 checkBtn.addEventListener('click', checkTriangle);
--- scripts/hypotenuse.js
+++ scripts/hypotenuse.js
@@ -3,8 +3,12 @@ const calculateBtn = document.querySelector("#check-btn");
 const outputBox = document.querySelector("#output-tag");
 
 function calculateSumOfValues(base, height){
+    if(base > 0 && height > 0){
     let sum = (base * base)+(height*height);
     return sum;
+}else{
+    outputBox.innerText = "The value's has to be greater than 0"
+}
 };
 
 function calculateHypotenuse(){
@@ -12,9 +16,11 @@ function calculateHypotenuse(){
         Number(userAngles[0].value),
         Number(userAngles[1].value)
     )
+    if(sumOfSquare){
     let theHypotenuse = Math.sqrt(sumOfSquare);
     outputBox.innerHTML = `The hypotenuse of your triangle is ${theHypotenuse.toFixed(3)}`;
     console.log(`The hypotenuse of your triangle is ${theHypotenuse.toFixed(3)}`);
+};
 }
 
 calculateBtn.addEventListener('click', calculateHypotenuse);
